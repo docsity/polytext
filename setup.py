e6fd9f150b45c8f55b8a0c0ea720c7c27335b5e3
marcodelgiudice <marco.dg@docsity.com>
2025-03-05 17:45:13 +0100

    Updated polytext version for deploy

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def get_requirements(*requirements_file):
 
 
 setup(name='polytext',
-      version='0.1.0',
+      version='0.1.1',
       url='https://github.com/docsity/polytext',
       # download_url='https://github.com/pualien/py-polytext/archive/0.1.23.tar.gz',
       license='MIT',
