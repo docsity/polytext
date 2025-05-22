f5504f7f5a2475cc82f3dd5eae047d3f7976e39c
marcodelgiudice <marco.dg@docsity.com>
2025-05-22 16:30:23 +0200

    Updated loaders __init__.py

--- polytext/loader/__init__.py
+++ polytext/loader/__init__.py
@@ -4,5 +4,6 @@ from .video import VideoLoader
 from .audio import AudioLoader
 from .youtube import YoutubeTranscriptLoader
 from .html import HtmlLoader
+from .ocr import OCRLoader
 
-__all__ = ['get_document_text', 'extract_text_from_file', 'TextLoader', 'VideoLoader', 'AudioLoader', 'HtmlLoader', 'YoutubeTranscriptLoader']
+__all__ = ['get_document_text', 'extract_text_from_file', 'TextLoader', 'VideoLoader', 'AudioLoader', 'HtmlLoader', 'YoutubeTranscriptLoader', 'OCRLoader']
