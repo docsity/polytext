3042785b78f1639c2539fedffba2a1578dd6c8f4
marcodelgiudice <marco.dg@docsity.com>
2025-06-05 11:50:09 +0200

    Updated library version to 0.1.3b3 for deploy to pypi

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_requirements(*requirements_file):
 
 setup(
     name='polytext',
-    version='0.1.3b2',
+    version='0.1.3b3',
     url='https://github.com/docsity/polytext',
     # download_url='https://github.com/pualien/py-polytext/archive/0.1.23.tar.gz',
     license='MIT',
