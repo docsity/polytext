b6286c793a1251e7c057ccfe0f4529d67ef3c8f9
marcodelgiudice <marco.dg@docsity.com>
2026-05-12 09:13:41 +0200

    Updated version for release

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_requirements(*requirements_file):
 
 setup(
     name='polytext',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/docsity/polytext',
     # download_url='https://github.com/pualien/py-polytext/archive/0.1.23.tar.gz',
     license='MIT',
